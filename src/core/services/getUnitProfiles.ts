9d8f921acd4be3496925828bf60eb918164d2e72
Phumrapee Limpianchop <git@rayriffy.com>
2020-11-07 08:56:36 +0900

    fix: api

--- src/core/services/getUnitProfiles.ts
+++ src/core/services/getUnitProfiles.ts
@@ -2,6 +2,4 @@ import { apiInstance } from './apiInstance'
 
 import { UnitProfile } from '../../@types/UnitProfile'
 
-export const getUnitProfiles = apiInstance<UnitProfile[]>(
-  'https://raw.githubusercontent.com/Sekai-World/sekai-master-db-diff/master/unitProfiles.json'
-)
+export const getUnitProfiles = apiInstance<UnitProfile[]>('unitProfiles.json')
