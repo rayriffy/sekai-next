c7c17c3e3db0b674e187d8f792ae6014689828a4
Phumrapee Limpianchop <git@rayriffy.com>
2020-11-09 07:22:41 +0900

    chore: staged changes

--- src/modules/cards/components/listing.tsx
+++ src/modules/cards/components/listing.tsx
@@ -14,7 +14,11 @@ export const CardsListing: FunctionComponent<Props> = props => {
     <div className="p-6">
       <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
         {cards.map(card => (
-          <CharacterCard key={`card-listing-${card.id}`} card={card} />
+          <CharacterCard
+            key={`card-listing-${card.id}`}
+            card={card}
+            afterTraining={card.rarity >= 3}
+          />
         ))}
       </div>
     </div>
