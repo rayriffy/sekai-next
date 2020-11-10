d9b38660130a4309b9c982881b800aba0f7550d8
Phumrapee Limpianchop <git@rayriffy.com>
2020-11-11 02:25:04 +0900

    chore: init event page

--- src/core/components/eventCard.tsx
+++ src/core/components/eventCard.tsx
@@ -15,17 +15,52 @@ import { Event } from '../../@types/Event'
 
 interface Props {
   event: Event
+  disableSlug?: boolean
+}
+
+interface DateSlugProps {
+  startAt: number
+  aggregateAt: number
 }
 
 export const EventCard: FunctionComponent<Props> = memo(props => {
-  const { event } = props
+  const { event, disableSlug = false } = props
+
+  return (
+    <Link href={`/event/${event.id}`}>
+      <a>
+        <div className="relative rounded-md overflow-hidden">
+          {!disableSlug && (
+            <DateSlug startAt={event.startAt} aggregateAt={event.aggregateAt} />
+          )}
+          <div className="absolute bottom-4 left-2 z-10 w-1/2">
+            <Image
+              src={getEventLogo(event.assetbundleName)}
+              alt={`${event.name} Logo`}
+              unsized
+            />
+          </div>
+          <Image
+            src={getEventBackground(event.assetbundleName)}
+            width={2048}
+            height={1261}
+            alt={`${event.name} Background`}
+          />
+        </div>
+      </a>
+    </Link>
+  )
+})
+
+const DateSlug: FunctionComponent<DateSlugProps> = memo(props => {
+  const { startAt, aggregateAt } = props
 
   const eventStatus = useMemo(() => {
     dayjs.extend(relativeTime)
 
     const currentTime = dayjs()
-    const eventStarted = dayjs(event.startAt)
-    const eventEnded = dayjs(event.aggregateAt)
+    const eventStarted = dayjs(startAt)
+    const eventEnded = dayjs(aggregateAt)
 
     if (currentTime.isAfter(eventStarted) && currentTime.isBefore(eventEnded)) {
       return {
@@ -46,35 +81,16 @@ export const EventCard: FunctionComponent<Props> = memo(props => {
   }, [])
 
   return (
-    <Link href={`/event/${event.id}`}>
-      <a>
-        <div className="relative rounded-md overflow-hidden">
-          <div
-            className={`absolute top-0 right-0 z-10 px-2 py-1 text-sm font-bold text-white rounded-bl-md uppercase ${
-              eventStatus.code === 0
-                ? 'bg-green-600'
-                : eventStatus.code === 1
-                ? 'bg-blue-600'
-                : 'bg-orange-600'
-            }`}
-          >
-            {eventStatus.text}
-          </div>
-          <div className="absolute bottom-4 left-2 z-10 w-1/2">
-            <Image
-              src={getEventLogo(event.assetbundleName)}
-              alt={`${event.name} Logo`}
-              unsized
-            />
-          </div>
-          <Image
-            src={getEventBackground(event.assetbundleName)}
-            width={2048}
-            height={1261}
-            alt={`${event.name} Background`}
-          />
-        </div>
-      </a>
-    </Link>
+    <div
+      className={`absolute top-0 right-0 z-10 px-2 py-1 text-sm font-bold text-white rounded-bl-md uppercase ${
+        eventStatus.code === 0
+          ? 'bg-green-600'
+          : eventStatus.code === 1
+          ? 'bg-blue-600'
+          : 'bg-orange-600'
+      }`}
+    >
+      {eventStatus.text}
+    </div>
   )
 })
--- src/pages/event/[id].tsx
+++ src/pages/event/[id].tsx
@@ -2,7 +2,10 @@ import { Fragment } from 'react'
 
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next'
 
+import dayjs from 'dayjs'
+
 import { HeadTitle } from '../../core/components/headTitle'
+import { EventCard } from '../../core/components/eventCard'
 
 import { Event } from '../../@types/Event'
 
@@ -16,7 +19,26 @@ const Page: NextPage<Props> = props => {
   return (
     <Fragment>
       <HeadTitle title={event.name} />
-      <div className="p-8 text-sm text-gray-900">{JSON.stringify(props)}</div>
+      <div className="max-w-7xl mx-auto p-4 sm:p-6 lg:p-8">
+        <div className="grid grid-cols-5 gap-6">
+          <div className="col-span-2">
+            <EventCard event={event} disableSlug />
+          </div>
+          <div className="col-span-3 space-y-2">
+            <h1 className="text-gray-900 font-bold text-2xl">{event.name}</h1>
+            <div>
+              <p>
+                Event start at{' '}
+                <b>{dayjs(event.startAt).format('DD MMMM YYYY HH:MM')}</b>
+              </p>
+              <p>
+                Event ended at{' '}
+                <b>{dayjs(event.aggregateAt).format('DD MMMM YYYY HH:MM')}</b>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </Fragment>
   )
 }
