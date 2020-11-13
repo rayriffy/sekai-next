d9a531a3bdc7ccc8bbf7bc2c8f2e5c707440bf1c
Phumrapee Limpianchop <git@rayriffy.com>
2020-11-14 07:37:51 +0900

    refactor: staged changes

---
+++ src/modules/event/detail.tsx
@@ -0,0 +1,46 @@
+import { FunctionComponent } from 'react'
+
+import dayjs from 'dayjs'
+
+import { EventCard } from '../../core/components/eventCard'
+
+import { Event } from '../../@types/Event'
+import { Honor } from '../../@types/Honor'
+import { ResourceBox } from '../../@types/ResourceBox'
+
+interface Props {
+  event: Event
+  honors: Honor[]
+  resourceBoxes: ResourceBox[]
+}
+
+export const EventDetail: FunctionComponent<Props> = props => {
+  const { event, honors, resourceBoxes } = props
+
+  return (
+    <div className="max-w-7xl mx-auto p-4 sm:p-6 lg:p-8">
+      <div className="grid grid-cols-1 md:grid-cols-5 gap-6">
+        <div className="col-span-1 md:col-span-2">
+          <EventCard event={event} disableSlug />
+        </div>
+        <div className="col-span-1 md:col-span-3 space-y-2">
+          <h1 className="text-gray-900 font-bold text-2xl">{event.name}</h1>
+          <div>
+            <p>
+              Event start at{' '}
+              <b>{dayjs(event.startAt).format('DD MMMM YYYY HH:MM')}</b>
+            </p>
+            <p>
+              Event ended at{' '}
+              <b>{dayjs(event.aggregateAt).format('DD MMMM YYYY HH:MM')}</b>
+            </p>
+            OK
+          </div>
+        </div>
+      </div>
+      <div className="pt-6">
+        <h2 className="font-bold text-2xl">Rewards</h2>
+      </div>
+    </div>
+  )
+}
--- src/pages/event/[id].tsx
+++ src/pages/event/[id].tsx
@@ -2,10 +2,8 @@ import { Fragment } from 'react'
 
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next'
 
-import dayjs from 'dayjs'
-
 import { HeadTitle } from '../../core/components/headTitle'
-import { EventCard } from '../../core/components/eventCard'
+import { EventDetail } from '../../modules/event/detail'
 
 import { Event } from '../../@types/Event'
 import { Honor } from '../../@types/Honor'
@@ -27,30 +25,7 @@ const Page: NextPage<Props> = props => {
   return (
     <Fragment>
       <HeadTitle title={event.name} />
-      <div className="max-w-7xl mx-auto p-4 sm:p-6 lg:p-8">
-        <div className="grid grid-cols-1 md:grid-cols-5 gap-6">
-          <div className="col-span-1 md:col-span-2">
-            <EventCard event={event} disableSlug />
-          </div>
-          <div className="col-span-1 md:col-span-3 space-y-2">
-            <h1 className="text-gray-900 font-bold text-2xl">{event.name}</h1>
-            <div>
-              <p>
-                Event start at{' '}
-                <b>{dayjs(event.startAt).format('DD MMMM YYYY HH:MM')}</b>
-              </p>
-              <p>
-                Event ended at{' '}
-                <b>{dayjs(event.aggregateAt).format('DD MMMM YYYY HH:MM')}</b>
-              </p>
-              OK
-            </div>
-          </div>
-        </div>
-        <div className="pt-6">
-          <h2 className="font-bold text-2xl">Rewards</h2>
-        </div>
-      </div>
+      <EventDetail {...props} />
     </Fragment>
   )
 }
