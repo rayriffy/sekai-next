7657a0765dff9c1fe94b2f509fb2fc130c7b8c17
Phumrapee Limpianchop <git@rayriffy.com>
2021-01-12 00:42:24 +0900

    feat: ics with description

--- src/pages/api/virtualLiveFeed.ts
+++ src/pages/api/virtualLiveFeed.ts
@@ -31,12 +31,29 @@ const api: NextApiHandler = async (req, res) => {
       .map(live => {
         const sortedSchedules = sortBy(live.virtualLiveSchedules, 'seq')
 
+        const diffSeconds = dayjs(first(sortedSchedules).endAt).diff(
+          dayjs(first(sortedSchedules).startAt),
+          'second'
+        )
+
         return {
           uid: `psekai-virtual-live-${live.id}`,
-          start: dayjs(first(sortedSchedules).startAt).toDate(),
-          end: dayjs(last(sortedSchedules).endAt).toDate(),
+          start: dayjs(first(sortedSchedules).startAt)
+            .tz('Asia/Tokyo')
+            .toDate(),
+          end: dayjs(last(sortedSchedules).endAt).tz('Asia/Tokyo').toDate(),
           summary: live.name,
           url: `https://sekai.rayriffy.com/virtualLive/${live.id}`,
+          description: `Duration: ${Math.floor(diffSeconds / 60)} minutes ${
+            diffSeconds % 60
+          } seconds\n------\n\n${sortedSchedules
+            .map(
+              (schedule, i) =>
+                `Round ${i}: ${dayjs(schedule.startAt)
+                  .tz('Asia/Tokyo')
+                  .format('DD MMM YYYY HH:MM')} (JST)`
+            )
+            .join('\n')}`,
         }
       }),
   })
