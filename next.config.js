ac4bc058761452c0cc64ac461f41196aeca2284e
Phumrapee Limpianchop <git@rayriffy.com>
2022-07-30 07:16:00 +0900

    chore: remove next-offline

--- next.config.js
+++ next.config.js
@@ -31,13 +31,8 @@ module.exports = withPlugins([[withBundleAnalyzer]], {
     polyfillsOptimization: true,
     scrollRestoration: true,
   },
-  // next-offline configuration
   async rewrites() {
     return [
-      {
-        source: '/service-worker.js',
-        destination: '/_next/static/service-worker.js',
-      },
       {
         source: '/subscribe/virtual-lives.ics',
         destination: '/api/virtualLiveFeed',
@@ -60,87 +55,5 @@ module.exports = withPlugins([[withBundleAnalyzer]], {
         permanent: true,
       },
     ]
-  },
-  dontAutoRegisterSw: true,
-  workboxOpts: {
-    swDest: process.env.NEXT_EXPORT
-      ? 'service-worker.js'
-      : 'static/service-worker.js',
-    runtimeCaching: [
-      // if you are customizing your runtime cache rules, please note that the
-      // first item in the runtime cache configuration array MUST be "start-url"
-      {
-        // MUST be the same as "start_url" in manifest.json
-        urlPattern: '/',
-        // use NetworkFirst or NetworkOnly if you redirect un-authenticated user to login page
-        // use StaleWhileRevalidate if you want to prompt user to reload when new version available
-        handler: 'NetworkFirst',
-        options: {
-          // don't change cache name
-          cacheName: 'start-url',
-        },
-      },
-      {
-        urlPattern: /\/_next\/image\?url/i,
-        handler: 'StaleWhileRevalidate',
-        options: {
-          cacheName: 'next-image-assets',
-        },
-      },
-      {
-        urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
-        handler: 'CacheFirst',
-        options: {
-          cacheName: 'google-fonts',
-        },
-      },
-      {
-        urlPattern: /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
-        handler: 'StaleWhileRevalidate',
-        options: {
-          cacheName: 'static-font-assets',
-        },
-      },
-      {
-        urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
-        handler: 'StaleWhileRevalidate',
-        options: {
-          cacheName: 'static-image-assets',
-        },
-      },
-      {
-        urlPattern: /\.(?:js)$/i,
-        handler: 'StaleWhileRevalidate',
-        options: {
-          cacheName: 'static-js-assets',
-        },
-      },
-      {
-        urlPattern: /\.(?:css|less)$/i,
-        handler: 'StaleWhileRevalidate',
-        options: {
-          cacheName: 'static-style-assets',
-        },
-      },
-      {
-        urlPattern: /\.(?:json|xml|csv)$/i,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'static-data-assets',
-        },
-      },
-      {
-        urlPattern: /.*/i,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'others',
-          expiration: {
-            maxEntries: 32,
-            maxAgeSeconds: 24 * 60 * 60, // 24 hours
-          },
-          networkTimeoutSeconds: 10,
-        },
-      },
-    ],
   },
 })
